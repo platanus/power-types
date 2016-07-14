2759273ba4bb765efbb9a1683de870874d629a5f
Ignacio Baixas <iobaixas@gmail.com>
2016-07-14 18:37:18 -0400

    feat(generators): adds directory generators

---
+++ lib/generators/power_types/init_generator.rb
@@ -0,0 +1,11 @@
+module PowerTypes
+  class InitGenerator < Rails::Generators::Base
+    desc "This generator creates the folder structure for the power-types gem"
+    def create_folders
+      empty_directory "app/commands/"
+      empty_directory "app/services/"
+      empty_directory "app/utils/"
+      empty_directory "app/values/"
+    end
+  end
+end

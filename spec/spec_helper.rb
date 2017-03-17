2910184ee05adfa823e251bc2a22d9c8fd48dab2
Guillermo Moreno <gmq@guillermomoreno.cl>
2017-03-17 17:39:45 -0300

    chore(): configure coveralls, travis deploy and hound

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+require 'simplecov'
+require 'coveralls'
+
+formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter::new(formatters)
+
+SimpleCov.start do
+  add_filter { |src| !(src.filename =~ /lib/) }
+  add_filter "spec.rb"
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pry'
 require 'power_types'

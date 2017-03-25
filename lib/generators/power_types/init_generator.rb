496539ee9eebc6fd11e93136ea3740eea8ebe687
Leandro Segovia <ldlsegovia@gmail.com>
2017-03-25 17:38:03 -0300

    feat(generators): add rails observer generator

--- lib/generators/power_types/init_generator.rb
+++ lib/generators/power_types/init_generator.rb
@@ -4,6 +4,7 @@ module PowerTypes
     def create_folders
       empty_directory "app/commands/"
       empty_directory "app/services/"
+      empty_directory "app/observers/"
       empty_directory "app/utils/"
       empty_directory "app/values/"
     end

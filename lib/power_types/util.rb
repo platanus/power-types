10421e18ee8f74997f54383a3ec091e39d3829e5
Cristóbal  Griffero <cgriffero@miuandes.cl>
2019-01-31 12:46:05 -0300

    feat(observer): add support to before_commit and after_commit callbacks

--- lib/power_types/util.rb
+++ lib/power_types/util.rb
@@ -1,6 +1,6 @@
 module PowerTypes
   module Util
-    OBSERVABLE_EVENTS = [:create, :update, :save, :destroy]
+    OBSERVABLE_EVENTS = [:create, :update, :save, :destroy, :commit]
     OBSERVABLE_TYPES = [:before, :after]
   end
 end

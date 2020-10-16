63446eeca77f64b52411d2dbf378e02c9bccdf89
Leandro Segovia <ldlsegovia@gmail.com>
2020-10-16 15:33:01 -0300

    feat(): add presentable concern

--- lib/power_types.rb
+++ lib/power_types.rb
@@ -9,6 +9,7 @@ require "power_types/patterns/observer/observable"
 require "power_types/patterns/observer/observer"
 require "power_types/patterns/observer/trigger"
 require "power_types/patterns/presenter/base_presenter"
+require "power_types/patterns/presenter/presentable"
 
 module PowerTypes
 end

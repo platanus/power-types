f1842d5c92374db0224afba1b24f65d50d92e772
nicolasguridi <nguridi@uc.cl>
2023-03-31 15:40:42 -0300

    fix(presenters): handle decoration errors

--- spec/lib/presenter/base_presenter_spec.rb
+++ spec/lib/presenter/base_presenter_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 describe PowerTypes::BasePresenter do
   let(:view) { double(view_method: 2) }
   let(:decorated_object) { double }
-  let(:decorable_instance) { double(decorate: decorated_object) }
+  let(:instance_to_decorate) { double(decorate: decorated_object) }
   let(:params) do
     {
       simple_param: 1,
-      decorated_param: decorable_instance
+      decorated_param: instance_to_decorate
     }
   end
 
@@ -20,6 +20,16 @@ describe PowerTypes::BasePresenter do
   it { expect(presenter.simple_param).to eq(1) }
   it { expect(presenter.decorated_param).to eq(decorated_object) }
   it { expect(presenter.send(:h).view_method).to eq(2) }
+
+  context "when the instance can't be decorated" do
+    let(:instance_to_decorate) { double }
+
+    before do
+      allow(instance_to_decorate).to receive(:decorate).and_raise(NameError)
+    end
+
+    it { expect(presenter.decorated_param).to eq(instance_to_decorate) }
+  end
 
   context "with already defined presenter method" do
     let(:error) { "attribute simple_param already defined in presenter" }
